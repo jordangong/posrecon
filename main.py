430e0271e0391cb82edb19326626ede3607a1d5d
Jordan Gong <jordan.gong@protonmail.com>
2022-08-28 10:20:40 +0800

    New dedicated PosReconCLR repo

---
+++ main.py
@@ -0,0 +1,295 @@
+from argparse import ArgumentParser, BooleanOptionalAction
+from functools import partial
+
+import torch
+from pl_bolts.datamodules import ImagenetDataModule
+from pl_bolts.models.self_supervised.simclr import SimCLRTrainDataTransform, SimCLREvalDataTransform
+from pl_bolts.optimizers import linear_warmup_decay, LARS
+from pl_bolts.transforms.dataset_normalizations import imagenet_normalization
+from pytorch_lightning import Trainer, LightningModule
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from torch import nn
+
+from models import MaskedPosReconCLRViT
+
+
+class PosReconCLR(LightningModule):
+    def __init__(
+            self,
+            gpus: int,
+            num_samples: int,
+            batch_size: int,
+            num_nodes: int = 1,
+            img_size: int = 224,
+            patch_size: int = 16,
+            in_chans: int = 3,
+            embed_dim: int = 768,
+            depth: int = 12,
+            num_heads: int = 12,
+            mlp_ratio: int = 4,
+            proj_dim: int = 128,
+            warmup_epochs: int = 10,
+            max_epochs: int = 100,
+            mask_ratio: float = 0.75,
+            temperature: float = 0.1,
+            loss_ratio: float = 1.,
+            optimizer: str = "adam",
+            exclude_bn_bias: bool = False,
+            start_lr: float = 0.0,
+            learning_rate: float = 1e-3,
+            final_lr: float = 0.0,
+            weight_decay: float = 1e-6,
+            **kwargs
+    ):
+        super(PosReconCLR, self).__init__()
+        self.save_hyperparameters()
+
+        self.gpus = gpus
+        self.num_nodes = num_nodes
+        self.num_samples = num_samples
+        self.batch_size = batch_size
+
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.in_chans = in_chans
+        self.embed_dim = embed_dim
+        self.depth = depth
+        self.num_heads = num_heads
+        self.mlp_ratio = mlp_ratio
+        self.proj_dim = proj_dim
+
+        self.optim = optimizer
+        self.exclude_bn_bias = exclude_bn_bias
+        self.weight_decay = weight_decay
+        self.mask_ratio = mask_ratio
+        self.temperature = temperature
+        self.loss_ratio = loss_ratio
+
+        self.start_lr = start_lr
+        self.final_lr = final_lr
+        self.learning_rate = learning_rate
+        self.warmup_epochs = warmup_epochs
+        self.max_epochs = max_epochs
+
+        self.model = MaskedPosReconCLRViT(
+            img_size,
+            patch_size,
+            in_chans,
+            embed_dim,
+            depth,
+            num_heads,
+            mlp_ratio,
+            proj_dim,
+            partial(nn.LayerNorm, eps=1e-6),
+        )
+
+        # compute iters per epoch
+        global_batch_size = num_nodes * gpus * batch_size if gpus > 0 else batch_size
+        self.train_iters_per_epoch = num_samples // global_batch_size
+
+    def shared_step(self, batch):
+        (img1, img2, _), _ = batch
+        img = torch.cat((img1, img2))
+        return self.model(img, self.mask_ratio, self.temperature)
+
+    def training_step(self, batch, batch_idx):
+        latent, pos_embed_pred, proj_embed, loss_recon, loss_clr = self.shared_step(batch)
+
+        self.log_dict({
+            'loss': {
+                'recon/train': loss_recon,
+                'clr/train': loss_clr,
+            },
+            'norm/latent': latent.norm(dim=-1).mean(),
+            'norm/pos_embed_pred': pos_embed_pred.norm(dim=-1).mean(),
+            'norm/proj_embed': proj_embed.norm(dim=-1).mean(),
+        }, sync_dist=True)
+        return self.loss_ratio * loss_recon + loss_clr
+
+    def validation_step(self, batch, batch_idx):
+        (img1, img2, _), y = batch
+        img = torch.cat((img1, img2))
+        *_, loss_recon, loss_clr = self.shared_step(batch)
+        self.log_dict({
+            'loss': {
+                'recon/val': loss_recon,
+                'clr/val': loss_clr,
+            },
+        }, sync_dist=True)
+        return self.loss_ratio * loss_recon + loss_clr
+
+    @staticmethod
+    def exclude_from_wt_decay(named_params, weight_decay, skip_list=("bias", "bn")):
+        params = []
+        excluded_params = []
+
+        for name, param in named_params:
+            if not param.requires_grad:
+                continue
+            elif any(layer_name in name for layer_name in skip_list):
+                excluded_params.append(param)
+            else:
+                params.append(param)
+
+        return [
+            {"params": params, "weight_decay": weight_decay},
+            {
+                "params": excluded_params,
+                "weight_decay": 0.0,
+            },
+        ]
+
+    def configure_optimizers(self):
+        if self.exclude_bn_bias:
+            params = self.exclude_from_wt_decay(self.named_parameters(), weight_decay=self.weight_decay)
+        else:
+            params = self.parameters()
+
+        if self.optim == "lars":
+            optimizer = LARS(
+                params,
+                lr=self.learning_rate,
+                momentum=0.9,
+                weight_decay=self.weight_decay,
+                trust_coefficient=0.001,
+            )
+        elif self.optim == "adam":
+            optimizer = torch.optim.Adam(params, lr=self.learning_rate, weight_decay=self.weight_decay)
+
+        warmup_steps = self.train_iters_per_epoch * self.warmup_epochs
+        total_steps = self.train_iters_per_epoch * self.max_epochs
+
+        scheduler = {
+            "scheduler": torch.optim.lr_scheduler.LambdaLR(
+                optimizer,
+                linear_warmup_decay(warmup_steps, total_steps, cosine=True),
+            ),
+            "interval": "step",
+            "frequency": 1,
+        }
+
+        return [optimizer], [scheduler]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+
+        # model params
+        parser.add_argument("--img_size", default=224, type=int,
+                            help="input image size")
+        parser.add_argument("--patch_size", default=16, type=int,
+                            help="patch size")
+        parser.add_argument("--in_chans", default=3, type=int,
+                            help="number of in channels")
+        parser.add_argument("--embed_dim", default=768, type=int,
+                            help="embedding dimension")
+        parser.add_argument("--depth", default=12, type=int,
+                            help="number of Transformer blocks")
+        parser.add_argument("--num_heads", default=12, type=int,
+                            help="number of self-attention heads")
+        parser.add_argument("--mlp_ratio", default=4, type=int,
+                            help="Ratio of embedding dim to MLP dim")
+        parser.add_argument("--proj_dim", default=128, type=int,
+                            help="projection head output dimension")
+        parser.add_argument("--fp32", default=True, action=BooleanOptionalAction,
+                            help="use fp32 or fp16")
+
+        # transform params
+        parser.add_argument("--gaussian_blur", default=True,
+                            action=BooleanOptionalAction, help="add gaussian blur")
+        parser.add_argument("--jitter_strength", type=float, default=1.0,
+                            help="jitter strength")
+        parser.add_argument("--dataset", type=str, default="imagenet",
+                            help="dataset")
+        parser.add_argument("--data_dir", type=str, default="dataset",
+                            help="path to dataset")
+
+        # training params
+        parser.add_argument("--fast_dev_run", default=False, type=int)
+        parser.add_argument("--num_nodes", default=1, type=int,
+                            help="number of nodes for training")
+        parser.add_argument("--gpus", default=1, type=int,
+                            help="number of gpus to train on")
+        parser.add_argument("--num_workers", default=8, type=int,
+                            help="num of workers per GPU")
+        parser.add_argument("--optimizer", default="adam", type=str,
+                            help="choose between adam/lars")
+        parser.add_argument("--exclude_bn_bias", default=True,
+                            action=BooleanOptionalAction,
+                            help="exclude bn/bias from weight decay")
+        parser.add_argument("--max_epochs", default=100, type=int,
+                            help="number of total epochs to run")
+        parser.add_argument("--max_steps", default=-1, type=int,
+                            help="max steps")
+        parser.add_argument("--warmup_epochs", default=10, type=int,
+                            help="number of warmup epochs")
+        parser.add_argument("--batch_size", default=128, type=int,
+                            help="batch size per gpu")
+
+        parser.add_argument("--mask_ratio", default=0.75, type=float,
+                            help="mask ratio of patches")
+        parser.add_argument("--temperature", default=0.1, type=float,
+                            help="temperature parameter in InfoNCE loss")
+        parser.add_argument("--loss_ratio", default=1., type=float,
+                            help="weight of two losses")
+        parser.add_argument("--weight_decay", default=1e-6, type=float,
+                            help="weight decay")
+        parser.add_argument("--learning_rate", default=1e-3, type=float,
+                            help="base learning rate")
+        parser.add_argument("--start_lr", default=0, type=float,
+                            help="initial warmup learning rate")
+        parser.add_argument("--final_lr", type=float, default=1e-6,
+                            help="final learning rate")
+
+        return parser
+
+
+if __name__ == '__main__':
+    paser = ArgumentParser()
+    paser = PosReconCLR.add_model_specific_args(paser)
+    args = paser.parse_args()
+
+    if args.dataset == "imagenet":
+        normalization = imagenet_normalization()
+        dm = ImagenetDataModule(data_dir=args.data_dir,
+                                batch_size=args.batch_size,
+                                num_workers=args.num_workers)
+        args.num_samples = dm.num_samples
+        args.input_height = dm.dims[-1]
+    else:
+        raise NotImplementedError("other datasets have not been implemented till now")
+
+    dm.train_transforms = SimCLRTrainDataTransform(
+        input_height=args.input_height,
+        gaussian_blur=args.gaussian_blur,
+        jitter_strength=args.jitter_strength,
+        normalize=normalization,
+    )
+
+    dm.val_transforms = SimCLREvalDataTransform(
+        input_height=args.input_height,
+        gaussian_blur=args.gaussian_blur,
+        jitter_strength=args.jitter_strength,
+        normalize=normalization,
+    )
+
+    model = PosReconCLR(**args.__dict__)
+
+    lr_monitor = LearningRateMonitor(logging_interval="step")
+    model_checkpoint = ModelCheckpoint(save_last=True, save_top_k=1, monitor="clr/val")
+    callbacks = [model_checkpoint, lr_monitor]
+
+    trainer = Trainer(
+        max_epochs=args.max_epochs,
+        max_steps=args.max_steps,
+        devices=args.gpus if args.gpus > 0 else None,
+        num_nodes=args.num_nodes,
+        accelerator="gpu" if args.gpus > 0 else None,
+        strategy="ddp" if args.gpus > 1 else None,
+        sync_batchnorm=True if args.gpus > 1 else False,
+        precision=32 if args.fp32 else 16,
+        callbacks=callbacks,
+        fast_dev_run=args.fast_dev_run,
+    )
+
+    trainer.fit(model, datamodule=dm)
---
+++ models.py
@@ -0,0 +1,247 @@
+from pathlib import Path
+
+import math
+import torch
+import torch.nn.functional as F
+from timm.models.helpers import named_apply, checkpoint_seq
+from timm.models.layers import trunc_normal_
+from timm.models.vision_transformer import VisionTransformer, get_init_weights_vit, PatchEmbed, Block
+from torch import nn
+from typing import Callable
+
+from pos_embed import get_2d_sincos_pos_embed
+
+
+class SyncFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, tensor):
+        ctx.batch_size = tensor.shape[0]
+
+        gathered_tensor = [torch.zeros_like(tensor)
+                           for _ in range(torch.distributed.get_world_size())]
+
+        torch.distributed.all_gather(gathered_tensor, tensor)
+        gathered_tensor = torch.cat(gathered_tensor, 0)
+
+        return gathered_tensor
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.clone()
+        torch.distributed.all_reduce(grad_input,
+                                     op=torch.distributed.ReduceOp.SUM,
+                                     async_op=False)
+
+        idx_from = torch.distributed.get_rank() * ctx.batch_size
+        idx_to = (torch.distributed.get_rank() + 1) * ctx.batch_size
+        return grad_input[idx_from:idx_to]
+
+
+class MaskedPosReconCLRViT(nn.Module):
+    """
+    Masked contrastive learning Vision Transformer w/ positional reconstruction
+    Default params are from ViT-Base.
+    """
+
+    def __init__(
+            self,
+            img_size: int = 224,
+            patch_size: int = 16,
+            in_chans: int = 3,
+            embed_dim: int = 768,
+            depth: int = 12,
+            num_heads: int = 12,
+            mlp_ratio: int = 4,
+            proj_dim: int = 128,
+            norm_layer: Callable = nn.LayerNorm,
+    ):
+        """
+        Args:
+            img_size: input image size
+            patch_size: patch size
+            in_chans: number of in channels
+            embed_dim: embedding dimension
+            depth: number of Transformer blocks
+            num_heads: number of self-attention heads
+            mlp_ratio: MLP dimension ratio (mlp_dim = embed_dim * mlp_ratio)
+            proj_dim: projection head output dimension
+            norm_layer: normalization layer
+        """
+        super(MaskedPosReconCLRViT, self).__init__()
+
+        # Encoder
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        # Following DeiT-3, exclude pos_embed from cls_token
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim),
+                                      requires_grad=False)
+
+        self.blocks = nn.Sequential(*[
+            Block(embed_dim, num_heads, mlp_ratio,
+                  qkv_bias=True, norm_layer=norm_layer)
+            for _ in range(depth)
+        ])
+        self.norm = norm_layer(embed_dim)
+
+        # Position predictor (linear layer equiv.)
+        self.pos_decoder = nn.Conv1d(embed_dim, embed_dim, kernel_size=1)
+
+        # Projection head
+        self.proj_head = nn.Sequential(
+            nn.Linear(embed_dim, embed_dim),
+            nn.BatchNorm1d(embed_dim),
+            nn.GELU(),
+            nn.Linear(embed_dim, proj_dim),
+        )
+
+        self.init_weights()
+
+    def init_weights(self):
+        pos_embed = get_2d_sincos_pos_embed(
+            self.pos_embed.size(-1), int(self.patch_embed.num_patches ** .5)
+        )
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        # Init weights in convolutional layers like in MLPs
+        patch_conv_weight = self.patch_embed.proj.weight.data
+        pos_conv_weight = self.pos_decoder.weight.data
+        nn.init.xavier_uniform_(patch_conv_weight.view(patch_conv_weight.size(0), -1))
+        nn.init.xavier_uniform_(pos_conv_weight.view(pos_conv_weight.size(0), -1))
+
+        nn.init.normal_(self.cls_token, std=.02)
+
+        self.apply(self._init_other_weights)
+
+    def _init_other_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @staticmethod
+    def rand_shuffle(x, pos_embed):
+        batch_size, seq_len, embed_dim = x.size()
+        # pos_embed: [1, seq_len, embed_dim]
+        batch_pos_embed = pos_embed.expand(batch_size, -1, -1)
+        # batch_pos_embed: [batch_size, seq_len, embed_dim]
+        noise = torch.rand(batch_size, seq_len, device=x.device)
+        shuffled_indices = noise.argsort()
+        # shuffled_indices: [batch_size, seq_len]
+        expand_shuffled_indices = shuffled_indices.unsqueeze(-1).expand(-1, -1, embed_dim)
+        # expand_shuffled_indices: [batch_size, seq_len, embed_dim]
+        batch_shuffled_pos_embed = batch_pos_embed.gather(1, expand_shuffled_indices)
+        # batch_shuffled_pos_embed: [batch_size, seq_len, embed_dim]
+        return x + batch_shuffled_pos_embed
+
+    @staticmethod
+    def rand_mask(x, mask_ratio):
+        batch_size, seq_len, embed_dim = x.size()
+        visible_len = int(seq_len * (1 - mask_ratio))
+        noise = torch.rand(batch_size, seq_len, device=x.device)
+        shuffled_indices = noise.argsort()
+        # shuffled_indices: [batch_size, seq_len]
+        unshuffled_indices = shuffled_indices.argsort()
+        # unshuffled_indices: [batch_size, seq_len]
+        visible_indices = shuffled_indices[:, :visible_len]
+        # visible_indices: [batch_size, seq_len * mask_ratio]
+        expand_visible_indices = visible_indices.unsqueeze(-1).expand(-1, -1, embed_dim)
+        # expand_visible_indices: [batch_size, seq_len * mask_ratio, embed_dim]
+        x_masked = x.gather(1, expand_visible_indices)
+        # x_masked: [batch_size, seq_len * mask_ratio, embed_dim]
+
+        return x_masked, expand_visible_indices
+
+    def forward_encoder(self, x, mask_ratio):
+        x = self.patch_embed(x)
+
+        x = self.rand_shuffle(x, self.pos_embed)
+        # batch_size*2, seq_len, embed_dim
+        x, visible_indices = self.rand_mask(x, mask_ratio)
+        # batch_size*2, seq_len * mask_ratio, embed_dim
+
+        # Concatenate [CLS] tokens w/o pos_embed
+        cls_tokens = self.cls_token.expand(x.size(0), -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        # batch_size*2, 1 + seq_len * mask_ratio, embed_dim
+
+        x = self.blocks(x)
+        x = self.norm(x)
+
+        return x, visible_indices
+
+    def forward_pos_decoder(self, latent):
+        # Exchange channel and length dimension for Conv1d
+        latent = latent.permute(0, 2, 1)
+        pos_embed_pred = self.pos_decoder(latent)
+        # Restore dimension
+        pos_embed_pred = pos_embed_pred.permute(0, 2, 1)
+
+        return pos_embed_pred
+
+    def pos_recon_loss(self, batch_pos_embed_pred, vis_ids):
+        batch_size = batch_pos_embed_pred.size(0)
+        # self.pos_embed: [1, seq_len, embed_dim]
+        # batch_pos_embed_pred: [batch_size*2, seq_len, embed_dim]
+        # vis_ids: [batch_size*2, seq_len * mask_ratio, embed_dim]
+        batch_pos_embed_targ = self.pos_embed.expand(batch_size, -1, -1)
+        # batch_pos_embed_targ: [batch_size*2, seq_len, embed_dim]
+        # Only compute loss on visible patches
+        visible_pos_embed_targ = batch_pos_embed_targ.gather(1, vis_ids)
+        # visible_pos_embed_targ: [batch_size*2, seq_len * mask_ratio, embed_dim]
+        loss = F.mse_loss(batch_pos_embed_pred, visible_pos_embed_targ)
+        return loss
+
+    @staticmethod
+    def info_nce_loss(features, temp, eps=1e-6):
+        feat = F.normalize(features)
+        # feat: [batch_size*2, proj_dim]
+        feat = torch.stack(feat.chunk(2), dim=1)
+        # feat: [batch_size, 2, proj_dim]
+        if torch.distributed.is_available() and torch.distributed.is_initialized():
+            feat = SyncFunction.apply(feat)
+        # feat: [batch_size (* world_size), 2, proj_dim]
+        feat1, feat2 = feat[:, 0, :], feat[:, 1, :]
+        # feat{1,2}: [batch_size (* world_size), proj_dim]
+        feat = torch.cat((feat1, feat2))
+        # feat: [batch_size*2 (* world_size), proj_dim]
+
+        # All samples, filling diagonal to remove identity similarity ((2N)^2 - 2N)
+        all_sim = (feat @ feat.T).fill_diagonal_(0)
+        # all_sim: [batch_size*2 (* world_size), batch_size*2 (* world_size)]
+        all_ = torch.exp(all_sim / temp).sum(-1)
+        # all_: [batch_size*2 (* world_size)]
+
+        # Positive samples (2N)
+        pos_sim = (feat1 * feat2).sum(-1)
+        # pos_sim: [batch_size (* world_size)]
+        pos = torch.exp(pos_sim / temp)
+        # Following all samples, compute positive similarity twice
+        pos = torch.cat((pos, pos))
+        # pos: [batch_size*2 (* world_size)]
+
+        loss = -torch.log(pos / (all_ + eps)).mean()
+
+        return loss
+
+    def forward_loss(self, batch_pos_embed_pred, vis_ids, features, temp=0.1):
+        loss_recon = self.pos_recon_loss(batch_pos_embed_pred, vis_ids)
+        loss_clr = self.info_nce_loss(features, temp)
+        return loss_recon, loss_clr
+
+    def forward(self, img, mask_ratio=0.75, temp=0.01):
+        # img: [batch_size*2, in_chans, height, weight]
+        latent, vis_ids = self.forward_encoder(img, mask_ratio)
+        # latent: [batch_size*2, 1 + seq_len * mask_ratio, embed_dim]
+        pos_pred = self.forward_pos_decoder(latent[:, 1:, :])
+        # pos_pred: [batch_size*2, seq_len * mask_ratio, embed_dim]
+        feat = self.proj_head(latent[:, 0, :])
+        # reps: [batch_size*2, proj_dim]
+        loss_recon, loss_clr = self.forward_loss(pos_pred, vis_ids, feat, temp)
+        return latent, pos_pred, feat, loss_recon, loss_clr
+
