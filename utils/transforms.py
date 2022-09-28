e00c01b0ccfe46d0fd4147b90f5e84ff93d0c4de
Jordan Gong <jordan.gong@protonmail.com>
2022-09-28 19:56:26 +0800

    Implement element-wise $\sigma$ sampling in RandomSigmaGaussianBlur

--- utils/transforms.py
+++ utils/transforms.py
@@ -3,7 +3,7 @@ from typing import Optional, Dict, Any
 import kornia.augmentation as K
 import torch
 from kornia.constants import BorderType
-from kornia.filters import gaussian_blur2d
+from kornia.filters import filter2d_separable
 from torch import Tensor, nn
 
 
@@ -15,7 +15,6 @@ class RandomSigmaGaussianBlur(K.IntensityAugmentationBase2D):
     """
     Implements Gaussian blur as described in the SimCLR paper
     Adapted from torchvision and opencv to kornia
-    TODO: $\\sigma$ is currently batch-wise sampled, better to be element-wise sampled
     """
 
     def __init__(
@@ -44,6 +43,36 @@ class RandomSigmaGaussianBlur(K.IntensityAugmentationBase2D):
         )
         self.sigma_dist = torch.distributions.Uniform(sigma_min, sigma_max)
 
+    @staticmethod
+    def batch_gaussian(window_size: int, sigma: Tensor) -> Tensor:
+        device, dtype = sigma.device, sigma.dtype
+        x = torch.arange(window_size, device=device, dtype=dtype) - window_size // 2
+        if window_size % 2 == 0:
+            x = x + 0.5
+        gauss = torch.exp(-x.pow(2.0) / (2 * sigma ** 2).unsqueeze(-1))
+        return gauss / gauss.sum(-1, keepdim=True)
+
+    def get_batch_gaussian_kernel1d(self, kernel_size: int, sigma: Tensor) -> torch.Tensor:
+        if not isinstance(kernel_size, int) \
+                or (kernel_size % 2 == 0) \
+                or (kernel_size <= 0):
+            raise TypeError("kernel_size must be an odd positive integer. "
+                            "Got {}".format(kernel_size))
+        window_1d: torch.Tensor = self.batch_gaussian(kernel_size, sigma)
+        return window_1d
+
+    def batch_gaussian_blur2d(
+            self,
+            input: Tensor,
+            kernel_size: tuple[int, int],
+            sigma: tuple[Tensor, Tensor],
+            border_type: str = 'reflect',
+    ) -> torch.Tensor:
+        kernel_x: torch.Tensor = self.get_batch_gaussian_kernel1d(kernel_size[1], sigma[1])
+        kernel_y: torch.Tensor = self.get_batch_gaussian_kernel1d(kernel_size[0], sigma[0])
+        out = filter2d_separable(input, kernel_x, kernel_y, border_type)
+        return out
+
     def apply_transform(
             self,
             input: Tensor,
@@ -51,8 +80,8 @@ class RandomSigmaGaussianBlur(K.IntensityAugmentationBase2D):
             flags: Dict[str, Any],
             transform: Optional[Tensor] = None
     ) -> Tensor:
-        sigma = self.sigma_dist.sample().item()
-        return gaussian_blur2d(
+        sigma = self.sigma_dist.sample((input.size(0),))
+        return self.batch_gaussian_blur2d(
             input,
             kernel_size=(flags["kernel_size"], flags["kernel_size"]),
             sigma=(sigma, sigma),
